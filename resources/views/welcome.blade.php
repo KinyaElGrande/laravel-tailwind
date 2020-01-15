cc155e37a52d0388567a22458b4e3cbc2e695178
KinyaElGrande <kinya.francis.mumbi@gmail.com>
2020-01-15 13:59:26 +0300

    top navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,41 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tailwind Laravel</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card component">
-                <div class="card-header">Dashboard</div>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <script src="https://use.fontawesome.com/7dfd18fbdb.js"></script>
+
+
+</head>
+<body class="bg-grey-light font-sans">
+    <div class="bg-white">
+        <div class="container mx-auto flex items-center py-4">
+            <nav class="w-2/5">
+                <a href="#" class="top-nav-item"><i class="fa fa-home fa-lg"></i> Home</a>
+                <a href="#" class="top-nav-item"><i class="fa fa-bolt fa-lg"></i> Moments</a>
+                <a href="#" class="top-nav-item"><i class="fa fa-bell fa-lg"></i> Notifications</a>
+                <a href="#" class="top-nav-item"><i class="fa fa-envelope fa-lg"></i> Messages</a>
+            </nav>
+            <div class="w-1/5 text-center"><a href=""><i class="fa fa-twitter fa-lg text-blue"></i></a></div>
+            <div class="w-2/5 flex justify-end">
+                <div class="mr-4 relative">
+                    <input type="text" class="bg-grey-lighter h-8 px-4 py-2 text-xs w-48 rounded-full" placeholder="Search Twitter">
+                    <span class="flex items-centre absolute inset-y-0 right-0  mr-3"><i class="fa fa-search text-grey"></i></span>
+                </div>
+                <div class="mr-4">
+                    <a href=""><img src="/img/avataaars.png" alt="" class="h-8 w-8 rounded-full"></a>
+                </div>
                 <div>
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Welcome Page
+                    <button class="bg-teal hover:bg-teal-dark text-white font-medium py-2 px-4 rounded-full">Tweet</button>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    
+</body>
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,109 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        indigo: '#5c6ac4',
+        white: '#ffffff',
         blue: '#007ace',
         red: '#de3618',
-      }
+        grey: {
+          light: '#dae1e7',
+          lighter: '#f1f5f8',
+          default: '#e4eaef',
+          dark: '#8795a1',
+          darker: '#606f7b',
+        },
+        teal: '#4dc0b5',
+      },
+      textSizes: {
+        'xs': '.75rem',     // 12px
+        'sm': '.875rem',    // 14px
+        'base': '1rem',     // 16px
+        'lg': '1.125rem',   // 18px
+        'xl': '1.25rem',    // 20px
+        '2xl': '1.5rem',    // 24px
+        '3xl': '1.875rem',  // 30px
+        '4xl': '2.25rem',   // 36px
+        '5xl': '3rem',      // 48px
+        '5.5xl': '4.125rem',
+      },
+      fontWeights: {
+        'hairline': 100,
+        'thin': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+      width: {
+        '1/5': '20%',
+        '2/5': '40%',
+      },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     }
   },
-  variants: {},
+  variants: {
+    appearance: ['responsive'],
+    backgroundAttachment: ['responsive'],
+    backgroundColors: ['responsive', 'hover'],
+    backgroundPosition: ['responsive'],
+    backgroundRepeat: ['responsive'],
+    backgroundSize: ['responsive'],
+    borderColors: ['responsive', 'hover'],
+    borderRadius: ['responsive'],
+    borderStyle: ['responsive'],
+    borderWidths: ['responsive'],
+    cursor: ['responsive'],
+    display: ['responsive'],
+    flexbox: ['responsive'],
+    float: ['responsive'],
+    fonts: ['responsive'],
+    fontWeights: ['responsive', 'hover'],
+    height: ['responsive'],
+    leading: ['responsive'],
+    lists: ['responsive'],
+    margin: ['responsive'],
+    maxHeight: ['responsive'],
+    maxWidth: ['responsive'],
+    minHeight: ['responsive'],
+    minWidth: ['responsive'],
+    negativeMargin: ['responsive'],
+    opacity: ['responsive', 'hover'],
+    overflow: ['responsive'],
+    padding: ['responsive'],
+    pointerEvents: ['responsive'],
+    position: ['responsive'],
+    resize: ['responsive'],
+    shadows: ['responsive'],
+    svgFill: [],
+    svgStroke: [],
+    textAlign: ['responsive'],
+    textColors: ['responsive', 'hover'],
+    textSizes: ['responsive'],
+    textStyle: ['responsive', 'hover'],
+    tracking: ['responsive'],
+    userSelect: ['responsive'],
+    verticalAlign: ['responsive'],
+    visibility: ['responsive'],
+    whitespace: ['responsive'],
+    width: ['responsive'],
+    zIndex: ['responsive'],
+  },
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    require('postcss-nested'),
+    require('postcss-custom-properties'),
   ]
 }
